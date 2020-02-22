1ad43a2b3b49cbfbc6f7b741c3bb10222115fd64
Andrey Kropotin <tork713@gmail.com>
2020-02-22 08:10:41 +0700

    Дошёл до STEP_1.4

--- f_1.py
+++ f_1.py
@@ -1,8 +1,12 @@
 #STEP_1
 
-from IPython.display import clear_output
+#STEP_1.1
 
-def display_board(board):
+from IPython.display import clear_output
+# Из библиотеки IPython.display импортирую метод clear_output, который очищает
+# доску каждый раз перед созданием новой игры
+
+def display_board(board):    # Создаю доску
     clear_output()
     print('   |   |')
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
@@ -15,3 +19,38 @@ def display_board(board):
     print('   |   |')
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print('   |   |')
+
+
+#STEP_1.2
+
+# Создаю функцию выбора игроком крестика или нолика
+
+def player_input():
+    marker = ''
+    while not (marker == 'X' or marker == 'O'):
+        marker = input('Player 1: Do you want to be X or O?').upper()
+
+    if marker == 'X':
+        return ('X', 'O')
+    else:
+        return ('O', 'X')
+
+
+#STEP_1.3
+
+# Функция которая берет на доске маркер и назначает этот маркер на выбранную игроком позицию
+
+def place_marker(board, marker, position):
+    board[position] = marker
+
+# STEP_1.4
+
+def win_check(board, mark):
+    return ((board[7] == mark and board[8] == mark and board[9] == mark) or  # across the top
+            (board[4] == mark and board[5] == mark and board[6] == mark) or  # across the middle
+            (board[1] == mark and board[2] == mark and board[3] == mark) or  # across the bottom
+            (board[7] == mark and board[4] == mark and board[1] == mark) or  # down the middle
+            (board[8] == mark and board[5] == mark and board[2] == mark) or  # down the middle
+            (board[9] == mark and board[6] == mark and board[3] == mark) or  # down the right side
+            (board[7] == mark and board[5] == mark and board[3] == mark) or  # diagonal
+            (board[9] == mark and board[5] == mark and board[1] == mark))  # diagonal
